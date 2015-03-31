2624ad44dc142093869be139f98fce17af417905
paoesco <paolo.escobar@hubesco.com>
2015-03-31 20:03:58 +0200

    Mots de passe chiffrés

--- src/main/java/fr/fava/gestionnaire/application/UtilisateurService.java
+++ src/main/java/fr/fava/gestionnaire/application/UtilisateurService.java
@@ -10,34 +10,37 @@ import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author paoesco
  */
 @Stateless
 public class UtilisateurService {
-    
+
     @Inject
     private EntityManager entityManager;
-    
+
     public String create(AjouterUtilisateurDTO dto) {
         String password = generatePassword();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         Utilisateur entity = new Utilisateur(
                 dto.getLogin(),
                 dto.getEmail(),
-                password,
+                passwordEncoder.encode(password),
                 dto.getGroupe(),
                 dto.getNom(),
                 dto.getPrenom());
         entityManager.persist(entity);
         return password;
     }
-    
+
     public Utilisateur retrieve(String login) {
         Utilisateur entity = entityManager.find(Utilisateur.class, login);
         return entity;
     }
-    
+
     public List<RetrieveUtilisateursDTO> retrieve() {
         List<Utilisateur> entities = entityManager.createNamedQuery(Utilisateur.QUERY_LISTE_ALL).getResultList();
         return entities.stream().map(entity -> {
@@ -51,45 +54,47 @@ public class UtilisateurService {
             return dto;
         }).collect(Collectors.toList());
     }
-    
+
     public void update(Utilisateur entity) {
         entityManager.merge(entity);
     }
-    
+
     public void delete(String login) {
         Utilisateur bean = entityManager.find(Utilisateur.class, login);
         entityManager.remove(bean);
     }
-    
+
     public void changePassword(String login, String actualPassword, String newPassword) throws WrongPasswordException {
         Utilisateur entity = entityManager.find(Utilisateur.class, login);
-        if (!actualPassword.equals(entity.getPassword())) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        if (!passwordEncoder.matches(actualPassword, entity.getPassword())) {
             throw new WrongPasswordException();
         }
-        entity.setPassword(newPassword);
+        entity.setPassword(passwordEncoder.encode(newPassword));
     }
-    
+
     public String reinitPassword(String login) {
         String password = generatePassword();
         Utilisateur entity = entityManager.find(Utilisateur.class, login);
-        entity.setPassword(password);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        entity.setPassword(passwordEncoder.encode(password));
         // TODO PES : envoyer email
         return password;
     }
-    
+
     private String generatePassword() {
         return UUID.randomUUID().toString().substring(0, 8);
     }
-    
+
     public void lock(String login) {
         Utilisateur entity = entityManager.find(Utilisateur.class, login);
         entity.setEnabled(false);
-        
+
     }
-    
+
     public void unlock(String login) {
         Utilisateur entity = entityManager.find(Utilisateur.class, login);
         entity.setEnabled(true);
     }
-    
+
 }
