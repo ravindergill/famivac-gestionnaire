dcb59cc39ca750c58366ec37d7c70b3b43e22e6c
Paolo <pao.esco@outlook.com>
2015-04-19 17:02:03 +0200

    Ajout d'un menu Monitoring

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -5,3 +5,4 @@ INSERT INTO Commune VALUES ('30000', 'Montpellier');
 
 INSERT INTO Groupe VALUES ('ROLE_GESTIONNAIRE', 'Gestionnaire');
 INSERT INTO Groupe VALUES ('ROLE_ADMIN', 'Administrateur');
+INSERT INTO Groupe VALUES ('ROLE_MONITORING', 'Monitoring');
